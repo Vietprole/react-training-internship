efd0d6bcedce6eaaabe5ed8b387a494f261c1b86
Vietprole <phuvietdn@gmail.com>
2025-01-14 15:03:20 +0700

    Modify search logic
    Using useEffect to sync data to localStorage
    Use searchPhrase state to calculate the filtered notes

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,42 @@ import Sidebar from "./components/sidebar/Sidebar";
 import NoteBox from "./components/note-box/NoteBox";
 import SearchBar from "./components/search-bar/SearchBar";
 import DarkModeIcon from "/assets/dark-mode-icon.svg";
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const VARIANTS = ["primary", "secondary", "tertiary"];
 
 function getRandomVariant(prevVariant) {
-  const availableVariants = VARIANTS.filter(variant => variant !== prevVariant);
+  const availableVariants = VARIANTS.filter(
+    (variant) => variant !== prevVariant
+  );
   const randomIndex = Math.floor(Math.random() * availableVariants.length);
   return availableVariants[randomIndex];
 }
 
-function getNotes(){
-  const storedNotes = localStorage.getItem('notes');
+function getNotes() {
+  const storedNotes = localStorage.getItem("notes");
   if (!storedNotes) return [];
 
   const parsedNotes = JSON.parse(storedNotes);
   // Convert string back to Date object
-  return parsedNotes.map(note => ({
+  return parsedNotes.map((note) => ({
     ...note,
-    createdAt: new Date(note.createdAt)
+    createdAt: new Date(note.createdAt),
   }));
 }
 
 function App() {
   const [notes, setNotes] = useState(getNotes());
+  const [searchPhrase, setSearchPhrase] = useState("");
+  const filteredNotes = notes.filter((note) =>
+    note.content.includes(searchPhrase)
+  );
 
-  const saveNoteToLocalStorage = (notes) => {
-    localStorage.setItem('notes', JSON.stringify(notes));
-  }
+  // Sync notes to localStorage
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   const handleCreateNote = () => {
     const prevNote = notes[notes.length - 1];
@@ -39,53 +46,46 @@ function App() {
 
     const newNote = {
       id: uuidv4(),
-      content: '',
+      content: "",
       createdAt: new Date(),
       variant: getRandomVariant(prevVariant),
     };
 
     const newNotes = [...notes, newNote];
     setNotes(newNotes);
-    // Since state update is async, we need to pass the new notes to saveNoteToLocalStorage
-    saveNoteToLocalStorage(newNotes);
-  }
+  };
 
   const handleNoteChange = (id, newContent) => {
-    const updatedNotes = notes.map(note =>
+    const updatedNotes = notes.map((note) =>
       note.id === id ? { ...note, content: newContent } : note
     );
     setNotes(updatedNotes);
-    // Since state update is async, we need to pass the updated notes to saveNoteToLocalStorage
-    saveNoteToLocalStorage(updatedNotes);
   };
 
   const handleDeleteNote = (id) => {
-    const notesAfterDelete = notes.filter(note => note.id !== id);
+    const notesAfterDelete = notes.filter((note) => note.id !== id);
     setNotes(notesAfterDelete);
-    // Since state update is async, we need to pass the notes after delete to saveNoteToLocalStorage
-    saveNoteToLocalStorage(notesAfterDelete);
-  }
-
-  const handleSearch = (searchPhrase) => {
-    if (searchPhrase === '') {
-      setNotes(getNotes());
-      return;
-    }
-    setNotes(notes.filter(note => note.content.includes(searchPhrase)));
-  }
+  };
 
   return (
     <div className="App">
-      <Sidebar handleCreateNote={handleCreateNote}/>
+      <Sidebar handleCreateNote={handleCreateNote} />
       <div className="main">
         <div className="header">
-          <SearchBar onChange={handleSearch}/>
-          <img src={DarkModeIcon} alt="Dark mode icon" className="dark-mode-icon"/>
+          <SearchBar setSearchPhrase={setSearchPhrase} />
+          <img
+            src={DarkModeIcon}
+            alt="Dark mode icon"
+            className="dark-mode-icon"
+          />
         </div>
-        <h1><span>Hello, </span><span className="name">Ruy</span>! 👋🏼</h1>
+        <h1>
+          <span>Hello, </span>
+          <span className="name">Ruy</span>! 👋🏼
+        </h1>
         <p className="description">All your notes are here, in one place!</p>
         <div className="notes-container">
-          {notes.map((note) => (
+          {filteredNotes.map((note) => (
             <NoteBox
               key={note.id}
               content={note.content}
--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -2,18 +2,18 @@ import SearchIcon from "/assets/search-icon.svg";
 import PropTypes from "prop-types";
 import "./search-bar.css";
 
-function SearchBar({ onChange }) {
+function SearchBar({ setSearchPhrase }) {
 
   return (
     <div className="search-bar">
       <img src={SearchIcon} alt="Search icon" className="search-icon"/>
-      <input type="text" className="search-input" placeholder="Search Notes" onChange={(e) => onChange(e.target.value)}/>
+      <input type="text" className="search-input" placeholder="Search Notes" onChange={(e) => setSearchPhrase(e.target.value)}/>
     </div>
   );
 }
 
 SearchBar.propTypes = {
-  onChange: PropTypes.func.isRequired,
+  setSearchPhrase: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
