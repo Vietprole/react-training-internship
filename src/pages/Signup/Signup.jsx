283436e888f1f0431e7ccdcb4a613249994067cf
Vietprole <phuvietdn@gmail.com>
2025-02-11 15:52:12 +0700

    Extract Layout

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router";
 import AppNameIcon from "/assets/app-name-icon.svg";
 import LogoIcon from "/assets/logo.svg";
-import ManImage from "/assets/man-image.svg";
 import SubmitIcon from "/assets/submit-icon.svg";
 import Logo from "../../components/Logo/Logo";
 import styles from "./Signup.module.css";
@@ -10,52 +9,38 @@ function Signup() {
   const navigate = useNavigate();
 
   function handleSignUpButtonClick() {
-    navigate("/login");
+    navigate("/");
   }
 
   return (
     <div className={styles.container}>
-      <div className={styles.leftSection}>
-        <img className={styles.manImage} src={ManImage} alt="Man image" />
-        <h1>Keep life simple</h1>
-        <p>
-          Store all your notes in a simple and intuitive app that helps you
-          enjoy what is most important in life.
-        </p>
-      </div>
-      <div className={styles.rightSection}>
-        <div className={styles.logoContainer}>
-          <Logo src={LogoIcon} variant="large" />
-          <img
-            className={styles.appNameIcon}
-            src={AppNameIcon}
-            alt="App name icon"
-          />
-        </div>
-        <h2>Sign up</h2>
-        <input
-          className={styles.username}
-          type="text"
-          placeholder="Type your username"
+      <div className={styles.logoContainer}>
+        <Logo src={LogoIcon} variant="large" />
+        <img
+          className={styles.appNameIcon}
+          src={AppNameIcon}
+          alt="App name icon"
         />
-        <input
-          className={styles.password}
-          type="password"
-          placeholder="Type your password"
-        />
-        <button
-          className={styles.signUpButton}
-          type="submit"
-          onClick={handleSignUpButtonClick}
-        >
-          <img
-            className={styles.submitIcon}
-            src={SubmitIcon}
-            alt="Submit icon"
-          />
-          Sign up
-        </button>
       </div>
+      <h2>Sign up</h2>
+      <input
+        className={styles.username}
+        type="text"
+        placeholder="Type your username"
+      />
+      <input
+        className={styles.password}
+        type="password"
+        placeholder="Type your password"
+      />
+      <button
+        className={styles.signUpButton}
+        type="submit"
+        onClick={handleSignUpButtonClick}
+      >
+        <img className={styles.submitIcon} src={SubmitIcon} alt="Submit icon" />
+        Sign up
+      </button>
     </div>
   );
 }
